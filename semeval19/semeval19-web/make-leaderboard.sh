3a2bef6a3e32113bf302a9d27415b1631fbc53bb
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2019-02-21 22:05:22 +0100

    update

--- semeval19/semeval19-web/make-leaderboard.sh
+++ semeval19/semeval19-web/make-leaderboard.sh
@@ -4,13 +4,16 @@ cat $1 \
     } FILENAME == "leaderboard.html" {
       if ($0 ~ /^<tr/) {
         split($0, cells, "</td><td>")
-        names[cells[2]] = cells[3]
-        links[cells[2]] = cells[8]
+        codename = cells[2]
+        if (!(codename in names)) {
+          names[codename] = cells[3]
+          links[codename] = cells[8]
+        }
       }
     } FILENAME == "/dev/stdin" {
       if (!($1 in users)) {
         position += 1
-        printf "<tr><td>%d</td><td>%s</td><td>%s</td><td>%.3f</td><td>%.3f</td><td>%.3f</td><td>%.3f</td><td>%s</td></tr>\n", position, $1, names[$1], $5, $6, $7, $8, links[$1]
+        printf "<tr><td>%d</td><td>%s</td><td>%s</td><td>%.3f</td><td>%.3f</td><td>%.3f</td><td>%.3f</td><td>%s\n", position, $1, names[$1], $5, $6, $7, $8, links[$1]
         users[$1] = 1
       }
     }' leaderboard.html /dev/stdin
